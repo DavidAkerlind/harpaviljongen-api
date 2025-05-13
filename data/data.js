40ec4b7425f79902c6cdb8a17d3282d0f2acb86c
DavidAkerlind <david.akerlind@icloud.com>
2025-05-13 16:57:58 +0200

    feat: update opening hours format and change active status for menu item

--- data/data.js
+++ data/data.js
@@ -1,12 +1,12 @@
 export const data = {
 	openingHours: [
-		{ day: 'Måndag', hours: 'Stängt' },
-		{ day: 'Tisdag', hours: '11:00 - sent' },
-		{ day: 'Onsdag', hours: '11:00 - sent' },
-		{ day: 'Torsdag', hours: '11:00 - sent' },
-		{ day: 'Fredag', hours: '11:00 - sent' },
-		{ day: 'Lördag', hours: '11:00 - sent' },
-		{ day: 'Söndag', hours: '11:00 - sent' },
+		{ day: 'Monday', hours: { from: null, to: null } },
+		{ day: 'Tuesday', hours: { from: '11:00', to: 'sent' } },
+		{ day: 'Wednesday', hours: { from: '11:00', to: 'sent' } },
+		{ day: 'Thursday', hours: { from: '11:00', to: 'sent' } },
+		{ day: 'Friday', hours: { from: '11:00', to: 'sent' } },
+		{ day: 'Saturday', hours: { from: '11:00', to: 'sent' } },
+		{ day: 'Sunday', hours: { from: '11:00', to: 'sent' } },
 	],
 	menus: [
 		{
@@ -102,7 +102,7 @@ export const data = {
 				},
 				{
 					id: 3,
-					active: true,
+					active: false,
 					title: 'Burrata sallad',
 					description:
 						'Burrata, kronärtskocka, tomater, variation av grönt',
